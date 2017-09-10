1203f521610ec993964de67c6209d71214b3a9e8
kakisoft <kakisttab@gmail.com>
2017-09-11 08:30:44 +0900

    4th commit

--- roop.py
+++ roop.py
@@ -1,5 +1,9 @@
 list1 = [1,2,3,4,5]
 
+wcnt = 0
+while wcnt < 5:
+    wcnt += 1
+
 # リスト
 for num in list1:
     print(num)
@@ -18,5 +22,14 @@ for counter in range(3, 31, 3):
 
 # 10から開始し、デクリメント。最終出力値は 0(カウンタが-1になったら終了)
 for counter in range(10, -1, -1):
+    if counter == 3:
+        break     #ループ終了
+    if counter > 7:
+        continue  #ループの先頭に戻る
     print(counter)
+
+# enumrate
+countries = ["フランス","アメリカ","中国","ドイツ","日本"]
+for index, country in enumerate(countries):
+    print(str(index +1) + ":" , country)
 
