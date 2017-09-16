479030ce1a936ba5f343e1359447548886ea2d12
kakisoft <kakisttab@gmail.com>
2017-09-16 09:45:33 +0900

    8th commit

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,8 @@
 colors = {"red":"赤", "blue":"青", "yello":"黄"}
 print(colors)
 
-print(colors["red"]) # ⇒ 赤
-print(len(colors))   # ⇒ 3
+print(colors["red"]) #=> 赤
+print(len(colors))   #=> 3
 
 colors["greed"] = "緑"  # 追加
 del colors["red"]       # 削除
@@ -11,16 +11,25 @@ if "blue" in colors:
     print("blue ditected")
     
 # キー、値の一覧を取得
-print(colors.keys())   # ⇒ dict_keys(['blue', 'yello', 'greed'])
-print(colors.values()) # ⇒ dict_values(['青', '黄', '緑'])
+print(colors.keys())   #=> dict_keys(['blue', 'yello', 'greed'])
+print(colors.values()) #=> dict_values(['青', '黄', '緑'])
 # これらは「ビュー」と呼ばれる種類のオブジェクト。リストに変換するには以下。
 print(list(colors.keys()))   # ['blue', 'yello', 'greed']
 print(list(colors.values())) # ['青', '黄', '緑']
 
 # すべてのキーと値のペアを取得
-print(colors.items()) # ⇒ dict_items([('blue', '青'), ('yello', '黄'), ('greed', '緑')])
+print(colors.items()) #=> dict_items([('blue', '青'), ('yello', '黄'), ('greed', '緑')])
 
 # イテレート可
 for jap, eng in colors.items():
     print("{}:{}".format(jap,eng))
 
+#２つのリストから辞書を作成
+e_seasons = ["Spring","Summer","Autumn","Winter"]
+j_seasons = ["春","夏","秋","冬"]
+seasons1 = {e:j for (e,j) in zip(e_seasons, j_seasons)}
+print(seasons1) #=> {'Spring': '春', 'Summer': '夏', 'Autumn': '秋', 'Winter': '冬'}
+
+#辞書のキーと値を入れ替える
+seasons2 = {j:e for (e,j) in seasons1.items() }
+print(seasons2) #=> {'春': 'Spring', '夏': 'Summer', '秋': 'Autumn', '冬': 'Winter'}
---
+++ list_comprehensions.py
@@ -0,0 +1,50 @@
+"""
+＜基本式＞
+ [式 for 変数 in イテレート可能なオブジェクト]
+"""
+
+#基本操作
+list1 = []
+for num in range(1, 21, 2):
+    list1.append(num ** 2)
+
+print(list1) # => [1, 9, 25, 49, 81, 121, 169, 225, 289, 361]
+
+list2 = [num ** 2 for num in range(1, 21, 2)]
+print(list2) # => [1, 9, 25, 49, 81, 121, 169, 225, 289, 361]
+
+#リストからリストを生成
+dolls = [1, 5, 9.5, 100] #() と、タプルにしてもOK
+rate = 101
+yens = [doll * rate for doll in dolls]
+print(yens) #=> [101, 505, 959.5, 10100]
+
+#出力内容を編集
+weekdays = [day + "曜日" for day in "月火水木金土日"]
+print(weekdays) #=> ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日']
+
+"""
+＜条件を満たす要素を抽出＞
+ [式 for 変数 in イテレート可能なオブジェクト if 条件式]
+"""
+#基本操作
+nums1 = [1, 3, 7, 10 ,9 ,15 ,20 ,30]
+nums2 = [n for n in nums1 if (n % 3) == 0] #3の倍数のみを抽出
+print(nums2) #=> [3, 9, 15, 30]
+
+#指定した文字列を含む要素を取り出す
+address = ["東京都千代田区","千葉県船橋市","東京都杉並区","埼玉県大宮市","東京都町田市","東京都西東京市","東京都大田区","神奈川県横浜市"]
+tokyo = [town[3:] for town in address if town.startswith("東京都")]
+print(tokyo) #=> ['千代田区', '杉並区', '町田市', '西東京市', '大田区']
+
+#タプルを要素とするリストから条件に合う要素を抽出する
+names = [("田中一郎","男"),("山田健太","男"),("佐藤順子","女")]
+men = [n[0] for n in names if n[1]=="男"]
+print(men) #=> ['田中一郎', '山田健太']
+
+#辞書の内包表記
+#  {キー:値 for 変数 in イテレート可能なオブジェクト}
+colors = ["yello","pink","blue","green"]
+colors_dict = {color:1 for color in colors}
+print(colors_dict) #=> {'yello': 1, 'pink': 1, 'blue': 1, 'green': 1}
+
