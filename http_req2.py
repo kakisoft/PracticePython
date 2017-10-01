499bc5e4fb70bfe49e8010dd76a7b6fe47ae854d
kakisoft <kakisttab@gmail.com>
2017-10-02 03:58:11 +0900

    edit req2

--- http_req2.py
+++ http_req2.py
@@ -1,27 +1,25 @@
 import requests
 import doctest
 
-TARGET_URL = "http://google.co.jp/"
-
-#r = requests.get(TARGET_URL)
-#print(r.text)
-
+TARGET_URL = "http://challenge-your-limits.herokuapp.com/"
 
 print("aaa")
 
-def sample(query):
-    """ requests sample that use qiita search api
-    >>> 'title' in sample('python')
-    True
-    >>> 'totle' in sample('python')
-    False
-    """
-    q = {'q':  query}
-    r = requests.get('https://qiita.com/api/v1/search', params=q)
-    return list(r.json()[0].keys())
+def req_sample2():
+    # q = {'q':  query}
+    # r = requests.get('https://qiita.com/api/v1/search', params=q)
+    r1 = requests.get(TARGET_URL)
+    r2 = requests.post("http://httpbin.org/post", data = {"key":"value"})
+    r3 = requests.put("http://httpbin.org/put", data = {"key":"value"})
+    r4 = requests.delete("http://httpbin.org/delete")
+    r5 = requests.head("http://httpbin.org/get")
+    r6 = requests.options("http://httpbin.org/get")
+    print(r1.text)
 
 if __name__ == "__main__":
-    doctest.testmod()
+    # doctest.testmod()
+    #doctest.testmod()
+    req_sample2()
 
 
 print("bbb")
