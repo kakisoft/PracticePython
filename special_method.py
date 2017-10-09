254757c8ae5bf56b291e2e599a63fef43c296f65
kakisoft <kakisttab@gmail.com>
2017-10-09 14:06:56 +0900

    add special method

--- special_method.py
+++ special_method.py
@@ -1,6 +1,25 @@
-class SpecialMethod(object):
-    def __int__(self):
-        return 10
+class SpecialMethod():
+    # コンストラクタ
+    def __init__(self,value):
+        self.value = int(value)
 
-ret1 = SpecialMethod()
-print(ret1)
+    #====================================
+    # 以下、演算子オーバーロード用の機能？
+    #====================================
+    
+    # 加算する時にコールされる
+    def __add__(self, _self):
+        return SpecialMethod(self.value + _self.value)
+
+    # intメソッドをコールする時に呼び出される
+    def __int__(self):
+        return 999
+
+instance1 = SpecialMethod(3)
+instance2 = SpecialMethod(7)
+
+sp_result1 = instance1 + instance2
+print(sp_result1.value) #=> 10
+print(int(sp_result1))  #=> 999
+
+
