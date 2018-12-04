f0fd2c2edd2d0122a9b0992f558bf5f61d1e3404
kakisoft <kakisttab@gmail.com>
2018-12-05 08:59:21 +0900

    e

--- command_line_arg.py
+++ command_line_arg.py
@@ -3,13 +3,19 @@
 実行時引数
 
 python command_line_arg.py a b c
- #=> 第1引数：a  第2引数：b  第3引数：c
+ #=> 実行ファイル名：command_line_arg.py  
+ #   第1引数：a  
+ #   第2引数：b  
+ #   第3引数：c
+ #   要素数：4
 """
 import sys
 
 args = sys.argv
 
 print(args)
+print("実行ファイル名：" + args[0])
 print("第1引数：" + args[1])
 print("第2引数：" + args[2])
 print("第3引数：" + args[3])
+print("要素数：" + str(len(args)))
