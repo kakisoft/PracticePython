90323900b740c370458e09ce26b01a75dcd04a43
kakisoft <kakisttab@gmail.com>
2018-12-16 23:21:16 +0900

    e

---
+++ csv02.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# READ_FILE_FULL_PATH_1   = "resources/sample11.csv"
+
+# df = pd.read_csv(READ_FILE_FULL_PATH_1 )
+
+# print(df)       # show all column
+# print(df['A'])  # show 'A' column
---
+++ memo/pandas.md
@@ -0,0 +1,6 @@
+## pandas
+Python Data Analysis Library  
+https://pandas.pydata.org/
+```
+pip install pandas
+```
