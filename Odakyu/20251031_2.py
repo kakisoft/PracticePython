bb23e3f8a67cc4b1013d76faff9a30ec7cdc0105
kakisoft <kakisttab@gmail.com>
2025-10-26 19:37:48 +0900

    a

---
+++ Odakyu/20251031_2.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("原点")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+        case Point():
+            print("それ以外のどこか")
+        case _:
+            print("座標ではない")
